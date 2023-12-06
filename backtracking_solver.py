622c2931287b739b1da46b34104b8457a2251af7
sinasun <sinakhodaveisi@gmail.com>
2023-12-05 19:57:50 -0500

    :sparkles: Added argparse for getting inputs

--- backtracking_solver.py
+++ backtracking_solver.py
@@ -1,5 +1,6 @@
-def save_board_to_csv(filename, board): 
-    pass
+import argparse
+
+output_file = ""
 
 def save_solution_to_csv(filename, board):
     pass
@@ -13,20 +14,27 @@ def is_valid(board, row, col, num):
 def backtrack(board):
     return False, board
 
-def solve_sudoku(board):
-    is_solvabel, solution = backtrack(board)
-    if is_solvabel:
-        save_board_to_csv("board.csv", board)
-        save_solution_to_csv("board_solution.csv", board)
+def solve_sudoku(board, output_file):
+    solution = backtrack(board)
+    save_solution_to_csv(output_file, board)
 
-def read_boards(filename):
+def read_boards(filename, output_file):
     # TODO: read from file
     boards = {}
     for board in boards:
-        solve_sudoku(board)
+        solve_sudoku(board, output_file)
 
 def main():
-    read_boards("initial_boards.csv")
+    parser = argparse.ArgumentParser(prog="BacktrackSolver", description="Sudoku Solver using Backtracking algorithm", epilog="python3 backtracking_solver.py -i data/boards.csv -o data/solutions.csv")
+
+    parser.add_argument('-i', '--input', default="data/boards.csv", help="Input file of boards")
+    parser.add_argument('-o', '--output', default="data/solutions.csv", help="Output file for saving solutions")
+
+    args = parser.parse_args()
+    input_file = args.input
+    output_file = args.output
+
+    read_boards(input_file, output_file)
 
 if __name__ == "__main__":
     main()
